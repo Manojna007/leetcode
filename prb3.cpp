7b73a70f5775ed024ca61207aeef7eb5f1d2ee41
Manojna007 <73425819+Manojna007@users.noreply.github.com>
2021-11-11 17:33:27 +0530

    Update prb3.cpp

--- prb3.cpp
+++ prb3.cpp
@@ -17,5 +17,21 @@ public:
             ans.push_back(s[i]);
         }
         return ans;
+        
+       /* string ans = "";
+        if(strs.size()==0) return "";
+        
+        string s = strs[0]; 
+        
+        for(int i=0;i<s.size();i++){
+            for(int j=1;j<strs.size();j++){
+                if(i>=strs[j].size() || s[i] != strs[j][i]){
+                    return ans;
+                }
+            }
+            ans.push_back(s[i]);
+        }
+        return ans;
+    }*/
     }
 };
